d2f6330a7a3059818870c467c0543cbad0aa5b4a
Erik <bluefoxbarker@gmail.com>
2022-03-17 12:33:45 +0000

    death screen ui template

---
+++ Assets/Scripts/UI/DeathScreen.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathScreen : MonoBehaviour
+{
+    public void RetryButtonPressed()
+    {
+        
+    }
+
+    public void QuitButtonPressed()
+    {
+        
+    }
+}
