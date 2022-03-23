5b095b1ac3a603d6462f287a3c70b33f598fb2b4
Erik <bluefoxbarker@gmail.com>
2022-03-23 11:25:15 +0000

    database tables
    items
    gameProfile_items
    
    weapons BLOBs
    gameProfile_weapons

--- Assets/Scripts/DatabaseManager.cs
+++ Assets/Scripts/DatabaseManager.cs
@@ -26,8 +26,21 @@ public class DatabaseManager : MonoBehaviour
                 command.ExecuteNonQuery();
                 
                 //weapons
-                command.CommandText = "CREATE TABLE IF NOT EXISTS weapons (weaponID INT, displayName VARCHAR(20), firearmClass INT, bulletCapacity INT, projectilesWhenFired INT, projectileSpeed DOUBLE, baseAccuracy DOUBLE, fireRate DOUBLE, ejectCartridgeOnFire INT, gunSmokeOnFire INT, twoHanded INT, reloadAngle DOUBLE, shootingHandHoldingX FLOAT, shootingHandHoldingY FLOAT, firePointX FLOAT, firePointY FLOAT, otherHandHoldingX FLOAT, otherHandHoldingY FLOAT, loadBulletsPointX FLOAT, loadBulletsPointY FLOAT, bulletReleaseKeyX FLOAT, bulletReleaseKeyY FLOAT, cylinderLocationX FLOAT, cylinderLocationY FLOAT);";
+                command.CommandText = "CREATE TABLE IF NOT EXISTS weapons (weaponID INT,weaponSprite BLOB, bulletSprite BLOB, casingSprite BLOB, projectileSprite BLOB,displayName VARCHAR(20), firearmClass INT, bulletCapacity INT, projectilesWhenFired INT, projectileSpeed DOUBLE, baseAccuracy DOUBLE, fireRate DOUBLE, ejectCartridgeOnFire INT, gunSmokeOnFire INT, twoHanded INT, reloadAngle DOUBLE, shootingHandHoldingX FLOAT, shootingHandHoldingY FLOAT, firePointX FLOAT, firePointY FLOAT, otherHandHoldingX FLOAT, otherHandHoldingY FLOAT, loadBulletsPointX FLOAT, loadBulletsPointY FLOAT, bulletReleaseKeyX FLOAT, bulletReleaseKeyY FLOAT, cylinderLocationX FLOAT, cylinderLocationY FLOAT);";
                 command.ExecuteNonQuery();
+                
+                //items
+                command.CommandText = "CREATE TABLE IF NOT EXISTS items (itemName VARCHAR(20), myTier INT, itemSprite BLOB,itemBriefDescription VARCHAR(50), itemDescription VARCHAR(50), effectRevolvers INT, effectPistols INT, effectShotguns INT, effectRifles INT, extraDamage DOUBLE, reloadSpeedIncrease DOUBLE, fireRate DOUBLE, enableLaserPointer INT, movementSpeedBuff DOUBLE, jumpPowerBuff DOUBLE, rollCooldownDecrease DOUBLE, shopDiscount DOUBLE, damageResistance DOUBLE, dodgeChance DOUBLE, extraLivesToGive DOUBLE, increaseMaxHealth DOUBLE);";
+                command.ExecuteNonQuery();
+                
+                //gameProfile_weapons
+                command.CommandText = "CREATE TABLE IF NOT EXISTS gameProfiles_weapons (gameProfilesID INT, weaponsID INT);";
+                command.ExecuteNonQuery();
+                
+                //gameProfile_items
+                command.CommandText = "CREATE TABLE IF NOT EXISTS gameProfiles_items (gameProfilesID INT, itemsID INT);";
+                command.ExecuteNonQuery();
+
             }
             connection.Close();
         }
---
+++ Assets/Scripts/UI/EEditor/ProfileSelect.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileSelect : MonoBehaviour
+{
+    [SerializeField] private GameObject profileList;
+    
+    
+    
+    
+}
